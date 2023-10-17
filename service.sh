0d1503327c0e59192f4dd811aa8cc6fdcac83b3a
yu1chaofan <1416638454@qq.com>
2023-10-17 11:40:01 +0800

    尝试解决zt开机不能自动连接

--- service.sh
+++ service.sh
@@ -4,6 +4,14 @@ MODDIR=${0%/*}
 
 cd /data/adb/zerotier
 
+# 检测网络是否连接
+while ! ping -c 1 -W 1 qq.com; do
+    echo "等待网络连接..."
+    sleep 5
+done
+
+# 当网络连接后，执行脚本
 sh ./add_ip_rule.sh
 sh ./start.sh
+sleep 30
 sh ./join.sh
