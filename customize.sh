24ab2e64bd2c40139bfdb361ea9afd74f98b2d6f
eventlOwOp <34993620+eventlOwOp@users.noreply.github.com>
2024-05-06 19:28:01 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,6 +4,8 @@ SKIPUNZIP=1
 ASH_STANDALONE=0
 
 ZT_PATH=/data/adb/zerotier
+CONF_PATH=/sdcard/Android/zerotier
+CONF_NAME=network_id.txt
 
 ui_print "- unzip"
 unzip -o "${ZIPFILE}" -x 'META-INF/*' -d "${MODPATH}" >&2
@@ -18,6 +20,11 @@ mv ${MODPATH}/zerotier/* ${ZT_PATH}
 ln -s ${ZT_PATH}/zerotier-one ${ZT_PATH}/zerotier-cli
 ln -s ${ZT_PATH}/zerotier-one ${ZT_PATH}/zerotier-idtool
 
+ui_print "- create config dir"
+mkidir -p ${CONF_PATH}
+touch ${CONF_PATH}/${CONF_NAME}
+echo "1111111111111111" > ${CONF_PATH}/${CONF_NAME}
+
 ui_print "- clean up"
 rm -rf ${MODPATH}/customize.sh 2>/dev/null
 rm -rf ${MODPATH}/bin 2>/dev/null
@@ -30,5 +37,6 @@ set_perm_recursive ${MODPATH} 0 0 0755 0644
 ui_print "---------- information -----------"
 ui_print "- zerotier root: ${ZT_PATH}"
 ui_print "- zerotier version: $(grep_prop version ${TMPDIR}/module.prop)"
+ui_print "- config file path: ${CONF_PATH}/${CONF_NAME}"
 
 
